0e0928146b997f83ce196d38d84ce075491d9e65
Raep9717 <42880367+Raep9717@users.noreply.github.com>
2018-09-14 19:06:31 -0500

    Interface principal terminada. Inicio de programación

--- app/src/main/java/com/example/raul0/generarfc/MainActivity.java
+++ app/src/main/java/com/example/raul0/generarfc/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.raul0.generarfc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void generarRFC(View v){
+
+        //Tomar la primera letra y voc del appellido paterno
+        EditText apellidoP = (EditText) findViewById(R.id.editText5);
+
+        char primerLetraAP = (R.id.editText5).getText().charAt(0);
+
+        //Tomar la primera letra y voc del appellido Materno
+
+        //Tomar la primera letra del primer nombre
+
+        //Tomar los ultimos dos dígitos del año de nacimiento
+
+        //Tomar los ultimos dos dígitos del mes de nacimiento
+
+        //Tomar los ultimos dos dígitos del día de nacimiento
+
+        //Generar 3 caracteres (Letras mayúsculas o número)
+
+        //Concatenar
+
     }
 }
