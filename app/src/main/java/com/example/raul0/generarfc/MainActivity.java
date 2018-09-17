12319aa77bcb08af4816f6fb069251f1370cbaf9
Raep9717 <raul00179@gmail.com>
2018-09-17 14:32:21 +0000

    Metodo obtener datos y poner datos listo

--- app/src/main/java/com/example/raul0/generarfc/MainActivity.java
+++ app/src/main/java/com/example/raul0/generarfc/MainActivity.java
@@ -3,25 +3,54 @@ package com.example.raul0.generarfc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 
 
 public class MainActivity extends AppCompatActivity {
+    //Crear objetos EditText
+    EditText nombre;
+    EditText apellidoP;
+    EditText apellidoM;
+    //Crear objetos botones
+    Button BtnMostrar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombre = (EditText)findViewById(R.id.editTextNombre);
+        apellidoP = (EditText)findViewById(R.id.editTextApellidoP);
+        apellidoM = (EditText)findViewById(R.id.editTextApellidoM);
+
+        //Botones
+        BtnMostrar = (Button)findViewById(R.id.acepta);
+
+        //Obtener datos de los EditTex
+        BtnMostrar.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String nombres = nombre.getText().toString();
+                //El método obtiene el valor del EditText
+
+                //Este metodo lo pone texto en los EditText
+                apellidoM.setText(nombres);
+
+            }
+
+        });
     }
 
     public void generarRFC(View v){
 
         //Tomar la primera letra y voc del appellido paterno
-        EditText apellidoP = (EditText) findViewById(R.id.editText5);
 
-        char primerLetraAP = (R.id.editText5).getText().charAt(0);
+
+
 
         //Tomar la primera letra y voc del appellido Materno
 
