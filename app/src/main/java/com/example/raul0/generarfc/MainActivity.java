c0988195615f12250cd85cfe3d6271a03151a3df
Raep9717 <raul00179@gmail.com>
2018-09-17 19:09:32 +0000

    Avances en clase.

--- app/src/main/java/com/example/raul0/generarfc/MainActivity.java
+++ app/src/main/java/com/example/raul0/generarfc/MainActivity.java
@@ -21,50 +21,72 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //EditText
         nombre = (EditText)findViewById(R.id.editTextNombre);
         apellidoP = (EditText)findViewById(R.id.editTextApellidoP);
         apellidoM = (EditText)findViewById(R.id.editTextApellidoM);
 
         //Botones
-        BtnMostrar = (Button)findViewById(R.id.acepta);
+        BtnMostrar = (Button)findViewById(R.id.buttonMuestra);
 
         //Obtener datos de los EditTex
         BtnMostrar.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                String nombres = nombre.getText().toString();
-                //El método obtiene el valor del EditText
 
-                //Este metodo lo pone texto en los EditText
-                apellidoM.setText(nombres);
+                //Hacer un método que devuelva en masyúsculas,
+                //sin espacios al inicio o fin, sin espacios
+                //Validar apellido paterno
+                String apellidop = apellidoP.getText().toString().toUpperCase().trim();
+                //Sustituir letras acentuadas
+
+                char [] vocalesAcentuadas = {'Á','É','Í','Ó','Ú'};
+                char [] vocales = {'A','E','I','O','U'};
+
+                //Recorrer y sustituir las vocales acentuadas
+
+
+
+
 
             }
 
         });
+
+
     }
 
-    public void generarRFC(View v){
+    public void GenerarRFC(View v){
+        String apellidop = apellidoP.getText().toString().toUpperCase().trim();
+        char [] vocalesAcentuadas = {'Á','É','Í','Ó','Ú'};
+        char [] vocales = {'A','E','I','O','U'};
+        boolean encontrada = false;
 
-        //Tomar la primera letra y voc del appellido paterno
+        for (byte pos=0; pos < vocales.length;pos++){
 
+            if (apellidop.indexOf(vocales[pos]) >=0){
+                encontrada =true;
+                break;
+            }
+        }
 
-
-
-        //Tomar la primera letra y voc del appellido Materno
-
-        //Tomar la primera letra del primer nombre
-
-        //Tomar los ultimos dos dígitos del año de nacimiento
-
-        //Tomar los ultimos dos dígitos del mes de nacimiento
-
-        //Tomar los ultimos dos dígitos del día de nacimiento
-
-        //Generar 3 caracteres (Letras mayúsculas o número)
-
-        //Concatenar
-
+        
     }
+
+    public String edecuar (String cadena){
+        String cadAux = cadena.toUpperCase().trim();
+
+        char [] vocalesAcentuadas = {'Á','É','Í','Ó','Ú'};
+        char [] vocales = {'A','E','I','O','U'};
+
+        for (byte pos=0; pos < vocalesAcentuadas.length; pos++){
+            cadAux.replace(vocalesAcentuadas[pos],vocales[pos]);
+
+
+        }
+        return cadAux;
+    }
+
+
 }
